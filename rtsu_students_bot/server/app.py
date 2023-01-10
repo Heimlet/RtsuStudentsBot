4bbda959da3a345bb6c2cf490098e54f56e6ddfc
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-01-10 18:24:07 +0500

    Added 'users_router'

--- rtsu_students_bot/server/app.py
+++ rtsu_students_bot/server/app.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
 from ..config import Settings
+from .routers import users_router
 
 
 def app_factory(settings: Settings):
     """Builds & returns application."""
     built_app = FastAPI()
+
+    built_app.include_router(users_router)
