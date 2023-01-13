08ffee701d3e29956558853edc11513e04f5cc6c
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-01-13 21:55:10 +0500

    Reformatted code, added title, description and version for 'app'

--- rtsu_students_bot/server/app.py
+++ rtsu_students_bot/server/app.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 from ..config import Settings
 from .routers import users_router
 
+router = APIRouter()
+
 
 def app_factory(settings: Settings):
     """Builds & returns application."""
-    built_app = FastAPI()
+    built_app = FastAPI(
+        description="API for RTSU.Students bot.",
+        title="RTSU.Students bot API",
+        version="1.0"
+    )
 
     built_app.include_router(users_router)
