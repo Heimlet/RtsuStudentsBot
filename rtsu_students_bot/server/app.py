dc1503267bd5de79d2e862e0a09cc50a75e6e012
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-01-14 19:40:52 +0500

    Added 'startup_event_handler'

--- rtsu_students_bot/server/app.py
+++ rtsu_students_bot/server/app.py
@@ -1,12 +1,38 @@
+import logging
+
 from fastapi import FastAPI, APIRouter
 
-from ..config import Settings
+from ..config import settings
 from .routers import users_router
+from .database import engine
+from .models import Base
 
 router = APIRouter()
 
 
-def app_factory(settings: Settings):
+async def startup_event_handler():
+    # Firstly, configure logging
+    level = logging.INFO
+    logging.getLogger("uvicorn").handlers.clear()
+
+    if settings.logging.debug:
+        level = logging.DEBUG
+
+    logging.basicConfig(
+        level=level,
+        format=settings.logging.format
+    )
+    logging.info("Logging configured.")
+
+    # Configure database
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    logging.info("Database configured.")
+
+
+def app_factory() -> FastAPI:
     """Builds & returns application."""
     built_app = FastAPI(
         description="API for RTSU.Students bot.",
@@ -14,4 +40,11 @@ def app_factory(settings: Settings):
         version="1.0"
     )
 
+    built_app.add_event_handler("startup", startup_event_handler)
+
     built_app.include_router(users_router)
+
+    return built_app
+
+
+app = app_factory()
