81adf77cb2913f0aef1e15a89f5f45c61a613515
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-03-09 20:00:38 +0500

    Implemented new tests.

--- tests/rstu_api_test.py
+++ tests/rstu_api_test.py
@@ -6,8 +6,8 @@ from rtsu_students_bot.rtsu import RTSUApi
 pytest_plugins = ('pytest_asyncio',)
 
 TEST_DATA = {
-    "login": "YOUR LOGIN FOR TESTING",
-    "password": "YOUR PASSWORD FOR TESTING",
+    "login": "your login",
+    "password": "your pass",
 }
 
 
@@ -33,3 +33,53 @@ async def test_rtsu_login(rtsu_client: RTSUApi):
     resp = await rtsu_client.auth(TEST_DATA.get("login"), TEST_DATA.get("password"))
 
     assert resp.token is not None
+
+
+@pytest.mark.asyncio
+async def test_rtsu_profile_fetching(rtsu_client: RTSUApi):
+    """
+    Tests rtsu profile fetching
+    :param rtsu_client:
+    :return:
+    """
+
+    await rtsu_client.auth(TEST_DATA.get("login"), TEST_DATA.get("password"))
+
+    profile = await rtsu_client.get_profile()
+
+    assert profile is not None
+    assert profile.full_name is not None
+
+
+@pytest.mark.asyncio
+async def test_rtsu_academic_years_fetching(rtsu_client: RTSUApi):
+    """
+    Tests rtsu academic years fetching
+    :param rtsu_client:
+    :return:
+    """
+
+    await rtsu_client.auth(TEST_DATA.get("login"), TEST_DATA.get("password"))
+
+    years = await rtsu_client.get_academic_years()
+
+    assert type(years) == list
+    assert len(years) > 0
+
+
+@pytest.mark.asyncio
+async def test_rtsu_academic_year_subjects_fetching(rtsu_client: RTSUApi):
+    """
+    Tests rtsu academic year fetching
+    :param rtsu_client:
+    :return:
+    """
+
+    await rtsu_client.auth(TEST_DATA.get("login"), TEST_DATA.get("password"))
+
+    ac_years = await rtsu_client.get_academic_years()
+    year = ac_years[0].id
+    years = await rtsu_client.get_academic_year_subjects(year)
+
+    assert type(years) == list
+    assert len(years) > 0
