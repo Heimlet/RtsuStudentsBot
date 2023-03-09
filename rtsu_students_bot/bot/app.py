68e51a65410baaed34b903e176f317e7d4cd18a7
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-03-09 18:46:24 +0500

    Implemeted 'dispatcher_factory'

--- rtsu_students_bot/bot/app.py
+++ rtsu_students_bot/bot/app.py
@@ -1,2 +1,16 @@
 from aiogram import Bot, Dispatcher
-from aiogram.utils import executor
+
+from rtsu_students_bot.config import settings
+
+
+def dispatcher_factory() -> Dispatcher:
+    """
+    Builds & returns `Bot` with `Dispatcher`
+    """
+
+    bot = Bot(settings.bot.token)
+    dp = Dispatcher(bot)
+
+    # TODO(Ilyas): Setup handlers
+
+    return dp
