c0e8027ab2016056e6af8bded951bffd7ec81835
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-03-17 18:57:30 +0500

    Added logging, changed '/start' command desc.

--- rtsu_students_bot/bot/handlers/startup.py
+++ rtsu_students_bot/bot/handlers/startup.py
@@ -1,10 +1,12 @@
+import logging
+
 from aiogram import types, Dispatcher
 
 BOT_COMMANDS = [
     types.BotCommand(
         "start",
-        "Старт бота"
-    )
+        "Запуск бота"
+    ),
 ]
 
 
@@ -14,6 +16,12 @@ async def startup_handler(dp: Dispatcher):
     :return:
     """
 
+    logger = logging.getLogger("bot")
+
+    logger.info("Setting up commands.")
+
     await dp.bot.set_my_commands(
         BOT_COMMANDS
     )
+
+    logging.info("Starting bot.")
