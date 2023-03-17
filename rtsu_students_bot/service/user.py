d5f58935c8b9c0e32d00fe29b5fda966dfea917d
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-03-17 09:57:56 +0500

    Fixed values providing.

--- rtsu_students_bot/service/user.py
+++ rtsu_students_bot/service/user.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sqlalchemy import select, update
+from sqlalchemy import select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from rtsu_students_bot.models import User
@@ -93,22 +93,27 @@ async def update_user_token(
     :return:
     """
 
-    user = get_user_by_tg_id(session, telegram_id)
+    user = await get_user_by_tg_id(session, telegram_id)
 
     if not user:
-        raise UserNotFound("User not found.")
+        raise UserNotFound(f"User with telegram-id {telegram_id} not found.")
 
     is_authorized = token is not None
-    stmt = update(User).values(token=token, is_authorized=is_authorized)
 
+    stmt = update(User).where(
+        int(user.id) == User.id
+    ).values(
+        is_authorized=is_authorized,
+        token=token,
+    )
     await session.execute(stmt)
 
-    return await get_user_by_tg_id(session, telegram_id)
+    return await get_user_by_tg_id(session, user.telegram_id)
 
 
 async def update_user(
-        user_id: int,
         session: AsyncSession,
+        user_id: int,
         telegram_id: Optional[int] = None,
         full_name: Optional[str] = None,
 ) -> User:
@@ -123,21 +128,37 @@ async def update_user(
 
     user = await get_user_by_id(session, user_id)
 
-    if not user:
+    if user is None:
         raise UserNotFound(f"User with ID {user_id} not found.")
 
     stmt = update(User).where(User.id == user_id)
 
     if telegram_id is not None:
-        stmt.values(
+        stmt = stmt.values(
             telegram_id=telegram_id,
         )
 
     if full_name is not None:
-        stmt.values(
+        stmt = stmt.values(
             full_name=full_name
         )
 
     await session.execute(stmt)
 
     return await get_user_by_id(session, user_id)
+
+
+async def delete_user(session: AsyncSession, user_id: int):
+    """
+    Deletes `User` object
+    :param user_id:
+    :param session: An `AsyncSession` object
+    :return:
+    """
+
+    if await get_user_by_id(session, user_id) is None:
+        raise ValueError("Invalid user-id passed.")
+
+    stmt = delete(User).where(User.id == user_id)
+
+    await session.execute(stmt)
