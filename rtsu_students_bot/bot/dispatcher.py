f1d0aef22ffc57e7eb9bafacb4ef7925218e4e66
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-03-17 10:47:12 +0500

    Fixed parse-mode

--- rtsu_students_bot/bot/dispatcher.py
+++ rtsu_students_bot/bot/dispatcher.py
@@ -2,5 +2,5 @@ from aiogram import Bot, Dispatcher
 
 from rtsu_students_bot.config import settings
 
-bot = Bot(settings.bot.token)
+bot = Bot(settings.bot.token, parse_mode="html")
 dp = Dispatcher(bot)
