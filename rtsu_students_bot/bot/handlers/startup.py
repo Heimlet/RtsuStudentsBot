f5e7c17c3f1c07c12136454b0e7a4dea280b9465
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-03-17 19:11:46 +0500

    Added logging-configuration, separated code to functions.

--- rtsu_students_bot/bot/handlers/startup.py
+++ rtsu_students_bot/bot/handlers/startup.py
@@ -1,6 +1,8 @@
 import logging
 
-from aiogram import types, Dispatcher
+from aiogram import types, Dispatcher, Bot
+
+from rtsu_students_bot.config import settings
 
 BOT_COMMANDS = [
     types.BotCommand(
@@ -10,18 +12,39 @@ BOT_COMMANDS = [
 ]
 
 
+def configure_logging():
+    """
+    Configures logging
+    """
+
+    level = logging.INFO
+
+    if settings.logging.debug:
+        level = logging.DEBUG
+
+    logging.basicConfig(level=level, format=settings.logging.format)
+
+
+async def setup_commands(bot: Bot):
+    """
+    Configures bot-commands
+    """
+
+    logging.info("Setting up commands.")
+
+    await bot.set_my_commands(
+        BOT_COMMANDS
+    )
+
+
 async def startup_handler(dp: Dispatcher):
     """
     Startup handler
-    :return:
+    :param dp: A `Dispatcher` instance
     """
 
-    logger = logging.getLogger("bot")
+    configure_logging()
 
-    logger.info("Setting up commands.")
-
-    await dp.bot.set_my_commands(
-        BOT_COMMANDS
-    )
+    await setup_commands(dp.bot)
 
     logging.info("Starting bot.")
