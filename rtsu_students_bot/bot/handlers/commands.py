51cbedceabad2001c7a49c49d0e1040e2e43464a
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-03-18 21:12:18 +0500

    Passed new values for 'start.html'

--- rtsu_students_bot/bot/handlers/commands.py
+++ rtsu_students_bot/bot/handlers/commands.py
@@ -1,17 +1,25 @@
 from aiogram import types, Dispatcher
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from rtsu_students_bot.models import User
 from rtsu_students_bot.bot.keyboards import inline
 from rtsu_students_bot.template_engine import render_template
 
 
-async def start(message: types.Message):
+async def start(message: types.Message, user: User):
     """
     Handles `/start` cmd
+    :param user:
     :param message: A message
     """
 
     await message.reply(
-        text=render_template("start.html", user=message.from_user.full_name),
+        text=render_template(
+            "start.html",
+            user=message.from_user.full_name,
+            telegram_id=message.from_user.id,
+            user_id=user.id,
+        ),
         reply_markup=inline.start_keyboard_factory()
     )
 
