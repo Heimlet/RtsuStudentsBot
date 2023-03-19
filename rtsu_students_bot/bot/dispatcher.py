cf2ccf3f1f0aacf540f8cf2dd2a9d77f26c1f431
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-03-19 19:24:48 +0500

    Using 'MemoryStorage' for dispatcher.

--- rtsu_students_bot/bot/dispatcher.py
+++ rtsu_students_bot/bot/dispatcher.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from rtsu_students_bot.config import settings
 
 bot = Bot(settings.bot.token, parse_mode="html")
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
