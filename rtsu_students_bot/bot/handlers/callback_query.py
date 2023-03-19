8b729cc45601910930376d3466f2c15674f8c3eb
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-03-19 19:25:38 +0500

    Implemented 'auth_callback_processor', 'cancel_callback_processor' etc...

---
+++ rtsu_students_bot/bot/handlers/callback_query.py
@@ -0,0 +1,98 @@
+from aiogram import types, Dispatcher
+from aiogram.dispatcher import FSMContext
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from rtsu_students_bot.rtsu import RTSUApi
+from rtsu_students_bot.template_engine import render_template
+from rtsu_students_bot.models import User
+from rtsu_students_bot.bot.keyboards import callbacks
+from rtsu_students_bot.bot.states import AuthState
+
+from .core import start_auth, authorize_user
+
+
+async def auth_callback_processor(
+        query: types.CallbackQuery,
+        user: User
+):
+    """
+    Handles `callbacks.AUTH_CALLBACK`
+    :param query: A callback-query
+    :param user: An User
+    """
+
+    await start_auth(query, user)
+
+
+async def cancel_callback_processor(
+        query: types.CallbackQuery,
+        state: FSMContext
+):
+    """
+    Handles `callbacks.CANCELLATION_CALLBACK`
+    :param state: A current state (fsm-context)
+    :param query:
+    :return:
+    """
+
+    await query.message.delete()
+
+    await query.bot.send_message(
+        query.from_user.id,
+        text=render_template("cancellation.html")
+    )
+    await state.finish()
+
+
+async def credentials_confirmation_callback_processor(
+        query: types.CallbackQuery,
+        callback_data: dict,
+        db_session: AsyncSession,
+        user: User,
+        rtsu: RTSUApi,
+        state: FSMContext
+):
+    """
+    Processes `callbacks.CONFIRMATION_CALLBACK`
+    :param state: A current state (fsm-context)
+    :param rtsu: An initialized rtsu-api client
+    :param user: A user in database
+    :param db_session: `AsyncSession` object
+    :param query: A callback-query
+    :param callback_data: Callback's data
+    """
+
+    # If user clicks `Yes` - `1` will be passed
+    # If user clicks `No` - `0` will be passed
+    # So, all data will be represented as strings in telegram-callbacks
+    # For getting boolean some converting needed
+    # Firstly, we convert string to int, after, we convert this int to boolean
+    ok = bool(int(callback_data.get("ok")))
+
+    await query.answer()
+    await query.message.delete()
+
+    async with state.proxy() as data:
+        login = data.get("login")
+        password = data.get("password")
+
+    if ok:
+        await authorize_user(query, user, login, password, db_session, rtsu, state)
+    else:
+        await query.bot.send_message(
+            query.from_user.id,
+            text=render_template("auth.html", user=user)
+        )
+        await AuthState.first()
+
+
+def setup(dp: Dispatcher):
+    """
+    Registers callback-query handlers
+    :param dp: A `Dispatcher` instance
+    """
+    dp.register_callback_query_handler(auth_callback_processor, callbacks.AUTH_CALLBACK.filter())
+    dp.register_callback_query_handler(cancel_callback_processor, callbacks.CANCELLATION_CALLBACK.filter())
+    dp.register_callback_query_handler(
+        credentials_confirmation_callback_processor, callbacks.CONFIRMATION_CALLBACK.filter(), state=AuthState.confirm
+    )
