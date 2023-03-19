ccb77e0cc9fe3ca6537e54b812bb30edf3c66e31
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-03-19 19:24:06 +0500

    Implemented 'auth' command-handler & code-cleanup

--- rtsu_students_bot/bot/handlers/commands.py
+++ rtsu_students_bot/bot/handlers/commands.py
@@ -1,9 +1,10 @@
 from aiogram import types, Dispatcher
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from rtsu_students_bot.models import User
-from rtsu_students_bot.bot.keyboards import inline
+from rtsu_students_bot.bot.keyboards import inline, reply
 from rtsu_students_bot.template_engine import render_template
+
+from . import core
 
 
 async def start(message: types.Message, user: User):
@@ -13,15 +14,29 @@ async def start(message: types.Message, user: User):
     :param message: A message
     """
 
+    markup = reply.main_menu_factory()
+
+    if not user.is_authorized:
+        markup = inline.auth_keyboard_factory()
+
     await message.reply(
         text=render_template(
             "start.html",
-            user=message.from_user.full_name,
-            telegram_id=message.from_user.id,
-            user_id=user.id,
+            user=user,
+            telegram_user=message.from_user
         ),
-        reply_markup=inline.start_keyboard_factory()
+        reply_markup=markup,
     )
+
+
+async def auth(message: types.Message, user: User):
+    """
+    Handles `/auth` cmd
+    :param user: A User
+    :param message: A message
+    """
+
+    await core.start_auth(message, user)
 
 
 def setup(dp: Dispatcher):
@@ -31,3 +46,4 @@ def setup(dp: Dispatcher):
     :return:
     """
     dp.register_message_handler(start, commands=["start"])
+    dp.register_message_handler(auth, commands=["auth"])
