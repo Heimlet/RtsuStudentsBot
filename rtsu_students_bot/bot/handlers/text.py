d61d8de08effad5c1c950615852302bc0ced7819
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-03-20 11:42:21 +0500

    Implemented 'show_profile_hanlder', 'show_statistics_handler' and 'show_subjects_handler'

--- rtsu_students_bot/bot/handlers/text.py
+++ rtsu_students_bot/bot/handlers/text.py
@@ -4,10 +4,16 @@
 
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 
+from rtsu_students_bot.bot.filters import AuthorizationFilter
+from rtsu_students_bot.rtsu import RTSUApi
+from rtsu_students_bot.models import User
 from rtsu_students_bot.bot.states import AuthState
 from rtsu_students_bot.template_engine import render_template
 from rtsu_students_bot.bot.keyboards import inline
+
+from . import core
 
 
 async def login_handler(
@@ -57,10 +63,67 @@ async def password_handler(
     )
 
 
+async def show_profile_handler(
+        message: types.Message,
+        rtsu: RTSUApi,
+        user: User,
+):
+    """
+    Handles 'Show profile' request
+    :param message: A message
+    :param rtsu: Initialized RTSU API client
+    :param user: A user from db
+    :return:
+    """
+
+    await core.show_profile(message, rtsu, user)
+
+
+async def show_statistics_handler(
+        message: types.Message,
+        rtsu: RTSUApi,
+):
+    """
+    Shows user's statistics
+    :param message:
+    :param rtsu:
+    :return:
+    """
+
+    await core.show_statistics(message, rtsu)
+
+
+async def show_subjects_handler(
+        message: types.Message,
+        rtsu: RTSUApi
+):
+    """
+    Handles 'Show statistics' request
+    :param rtsu: Initialized RTSU API client
+    :param message: A message
+    :return:
+    """
+
+    await core.show_subjects(message, rtsu)
+
+
 def setup(dp: Dispatcher):
     """
     Setups text-handlers
     :param dp: A `Dispatcher` instance
     """
-    dp.register_message_handler(login_handler, state=AuthState.login, content_types=[types.ContentType.TEXT])
+    dp.register_message_handler(
+        login_handler,
+        state=AuthState.login,
+        content_types=[types.ContentType.TEXT],
+    )
     dp.register_message_handler(password_handler, state=AuthState.password, content_types=[types.ContentType.TEXT])
+    dp.register_message_handler(
+        show_profile_handler, Text(equals="🎓 Профиль"), AuthorizationFilter(authorized=True)
+    )
+    dp.register_message_handler(
+        show_statistics_handler, Text(equals="📊 Статистика"), AuthorizationFilter(authorized=True)
+    )
+    dp.register_message_handler(
+        show_subjects_handler, Text(equals="📕 Дисциплины"), AuthorizationFilter(authorized=True)
+    )
