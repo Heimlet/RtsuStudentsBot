5c790b31c1c33f252b7d1104525d75a116d89098
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-03-20 11:40:00 +0500

    Implemented 'show_profile', 'show_statistics' and 'show_subjects'

--- rtsu_students_bot/bot/handlers/core.py
+++ rtsu_students_bot/bot/handlers/core.py
@@ -2,14 +2,14 @@
 `core.py` - Core-functionality of bot
 """
 
-from typing import Union
+from typing import Union, List
 
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from rtsu_students_bot.service import user
-from rtsu_students_bot.rtsu import RTSUApi, exceptions
+from rtsu_students_bot.rtsu import RTSUApi, exceptions, schemas
 from rtsu_students_bot.bot.keyboards import inline, reply
 from rtsu_students_bot.bot.states import AuthState
 from rtsu_students_bot.models import User
@@ -41,6 +41,54 @@ async def start_auth(
         update.from_user.id,
         text=text,
         reply_markup=markup
+    )
+
+
+async def show_profile(
+        message: types.Message,
+        rtsu_client: RTSUApi,
+        user_in_db: User,
+):
+    """
+    Shows information about profile
+    :param message:
+    :param rtsu_client:
+    :param user_in_db:
+    :return:
+    """
+
+    profile = await rtsu_client.get_profile()
+
+    text = render_template(
+        "profile.html",
+        profile=profile,
+        user=user_in_db,
+        telegram_user=message.from_user
+    )
+
+    await message.bot.send_message(
+        message.from_user.id,
+        text,
+    )
+
+
+async def show_statistics(
+        message: types.Message,
+        rtsu_client: RTSUApi,
+):
+    """
+    Shows user's statistics
+    :param message: A message
+    :param rtsu_client: Initialized RTSU API client
+    :return:
+    """
+
+    current_year_id = await rtsu_client.get_current_year_id()
+    subjects: List[schemas.Subject] = await rtsu_client.get_academic_year_subjects(current_year_id)
+
+    await message.bot.send_message(
+        message.chat.id,
+        text=render_template("statistics.html", subjects=subjects)
     )
 
 
@@ -95,3 +143,24 @@ async def authorize_user(
         reply_markup=reply.main_menu_factory()
     )
     await state.finish()
+
+
+async def show_subjects(
+        message: types.Message,
+        rtsu_client: RTSUApi
+):
+    """
+    Shows user's subjects
+    :param message:
+    :param rtsu_client:
+    :return:
+    """
+
+    current_year = await rtsu_client.get_current_year_id()
+    subjects = await rtsu_client.get_academic_year_subjects(current_year)
+
+    await message.bot.send_message(
+        message.chat.id,
+        text="📕 Дисциплины",
+        reply_markup=inline.subjects_keyboard_factory(subjects)
+    )
