0cdfa50d2f097da29ff19fa84aded9908f99f40e
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-03-20 11:42:39 +0500

    Added 'SUBJECT_CALLBACK'

--- rtsu_students_bot/bot/keyboards/callbacks.py
+++ rtsu_students_bot/bot/keyboards/callbacks.py
@@ -3,3 +3,4 @@ from aiogram.utils.callback_data import CallbackData
 AUTH_CALLBACK = CallbackData("auth")
 CANCELLATION_CALLBACK = CallbackData("cancel")
 CONFIRMATION_CALLBACK = CallbackData("cnfrm", "ok")
+SUBJECT_CALLBACK = CallbackData("sbj", "id")
