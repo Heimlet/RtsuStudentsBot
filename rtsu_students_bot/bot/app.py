fe5fc4d6778b428db97e2ec0deb1bb3c3c4a4264
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-03-20 14:12:26 +0500

    Code cleanup.

--- rtsu_students_bot/bot/app.py
+++ rtsu_students_bot/bot/app.py
@@ -1,15 +1,22 @@
 from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from rtsu_students_bot.config import settings
 
-from . import handlers, middlewares, filters
-from .dispatcher import bot, dp
+from . import handlers, middlewares
 
 
 def get_app() -> Dispatcher:
     """
     Initializes & returns `Dispatcher`
     """
+
+    # Create bot & dispatcher
+
+    memory_storage = MemoryStorage()
+
+    bot = Bot(settings.bot.token, parse_mode="html")
+    dp = Dispatcher(bot, storage=memory_storage)
 
     # Setup handlers
 
