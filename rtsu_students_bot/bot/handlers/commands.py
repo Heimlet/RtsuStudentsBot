b88921659b71fcfd437bbbe02856cfcbcaf04701
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-03-20 14:04:39 +0500

    Implemented 'about', 'logout', 'profile' and 'subjects' command-handlers

--- rtsu_students_bot/bot/handlers/commands.py
+++ rtsu_students_bot/bot/handlers/commands.py
@@ -1,5 +1,7 @@
 from aiogram import types, Dispatcher
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from rtsu_students_bot.rtsu import RTSUApi
 from rtsu_students_bot.bot.filters import AuthorizationFilter
 from rtsu_students_bot.models import User
 from rtsu_students_bot.bot.keyboards import inline, reply
@@ -40,6 +42,66 @@ async def auth(message: types.Message, user: User):
     await core.start_auth(message, user)
 
 
+async def help_cmd(message: types.Message):
+    """
+    Handles `help` cmd
+    :param message: A message
+    """
+
+    await core.show_help(message)
+
+
+async def statistics(message: types.Message, rtsu: RTSUApi):
+    """
+    Handles `statistics` cmd
+    :param message: A message
+    :param rtsu: Initialized RTSU API client
+    """
+
+    await core.show_statistics(message, rtsu)
+
+
+async def subjects(message: types.Message, rtsu: RTSUApi):
+    """
+    Handles `subjects` cmd
+    :param message: A message
+    :param rtsu: Initialized RTSU API client
+    """
+
+    await core.show_subjects(message, rtsu)
+
+
+async def profile(message: types.Message, rtsu: RTSUApi, user: User):
+    """
+    Handles `profile` cmd
+    :param message: A message
+    :param user: User in db
+    :param rtsu: Initialized RTSU API client
+    """
+
+    await core.show_profile(message, rtsu, user)
+
+
+async def logout(message: types.Message, user: User, db_session: AsyncSession):
+    """
+    Handles `logout` cmd
+    :param db_session: `AsyncSession` object
+    :param user: A user in db
+    :param message: A message
+    """
+
+    await core.logout_user(message, user, db_session)
+
+
+async def about(message: types.Message):
+    """
+    Handles `about` cmd
+    :param message: A message
+    """
+
+    await core.show_about(message)
+
+
 def setup(dp: Dispatcher):
     """
     Setups commands-handlers
@@ -47,4 +109,11 @@ def setup(dp: Dispatcher):
     :return:
     """
     dp.register_message_handler(start, commands=["start"])
+    dp.register_message_handler(help_cmd, commands=["help"])
+    dp.register_message_handler(about, commands=["about"])
+    dp.register_message_handler(logout, AuthorizationFilter(True), commands=["logout"])
+    dp.register_message_handler(profile, AuthorizationFilter(True), commands=["profile"])
+    dp.register_message_handler(subjects, AuthorizationFilter(True), commands=["subjects"])
+    dp.register_message_handler(statistics, AuthorizationFilter(True), commands=["stat"])
+    dp.register_message_handler(auth, AuthorizationFilter(False), commands=["auth"])
     dp.register_message_handler(auth, AuthorizationFilter(authorized=True), commands=["auth"])
