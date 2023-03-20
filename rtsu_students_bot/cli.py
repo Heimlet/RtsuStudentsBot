d571ed56671f07b355ebd6b3aebd7002474d8790
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2023-03-20 14:47:22 +0500

    Implemented bot's starting via webhooks.

--- rtsu_students_bot/cli.py
+++ rtsu_students_bot/cli.py
@@ -1,6 +1,7 @@
 from aiogram import executor
 from typer import Typer, Argument
 
+from .config import settings
 from .bot import get_app, handlers
 
 typer_app = Typer()
@@ -9,7 +10,7 @@ typer_app = Typer()
 @typer_app.command()
 def start(
         skip_updates: bool = Argument(default=False, help="Skip telegram updates on start?"),
-        use_webhook: bool = Argument(default=False, help="Use webhook mode for receiving updates?")
+        use_webhook: str = Argument(default=None, help="Use webhook for receiving updates?")
 ):
     """
     Starts bot
@@ -20,11 +21,31 @@ def start(
     # Build bot
     dp = get_app()
 
+    # Build startup-handler
+
+    startup_handler = handlers.startup.startup_handler_factory()
+
     if use_webhook:
-        raise NotImplementedError
+        # Check for `webhook` settings are not `None`
+
+        if settings.webhooks is None:
+            print("Please, fill webhook's settings.")
+            exit(-1)
+
+        startup_handler = handlers.startup.startup_handler_factory(f"{settings.webhooks.host}{settings.webhooks.path}")
+
+        executor.start_webhook(
+            dispatcher=dp,
+            on_startup=startup_handler,
+            skip_updates=True,
+            host=settings.webhooks.webapp_host,
+            port=settings.webhooks.webapp_port,
+            webhook_path=settings.webhooks.path,
+            check_ip=True
+        )
     else:
         executor.start_polling(
             dp,
             skip_updates=skip_updates,
-            on_startup=handlers.startup.startup_handler
+            on_startup=startup_handler
         )
